5e9a428478ffd20166b21bc56141a939cd582492
tao.li <edisonbiti@gmail.com>
2018-01-21 23:13:30 +0800

    to_sql done

--- mock_trans.sql
+++ mock_trans.sql
@@ -1,9 +1,10 @@
 #CREATE DATABASE `auto_stock` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `auto_stock`;
-#drop table mock_trans;
+drop table mock_trans;
 CREATE TABLE `mock_trans` (
   `id` int(32) NOT NULL auto_increment COMMENT '',
   `code` varchar(128) NOT NULL COMMENT '',
+  `name` varchar(128) NOT NULL COMMENT '',
   `trans_type` varchar(8) NOT NULL COMMENT '',
   `price` float NOT NULL COMMENT '',
   `count` int(16) NOT NULL COMMENT '',
--- tushare_test.py
+++ tushare_test.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import tushare as ts
 import pandas as pd
+from datetime import datetime
 
 def profit_ratio_ordered():
  df = ts.profit_data(top=30)
@@ -12,13 +13,33 @@ def profit_ratio_ordered():
  select_df.to_csv('ts_test.result')
  print("write file done")
 
+def do_trans(df, engine):
+  df.to_sql('mock_trans', engine, if_exists='append', index=False)
+
 def select_share():
   df = pd.read_csv("today_all.csv")
+  #df = ts.get_today_all()
   # 获取股价上涨超过9%的股票
-  selected_codes = df[df['changepercent'] > 9][['code','name','changepercent']].sort_values('changepercent', ascending=False)
+  selected_codes = df[df['changepercent'] > 10][['code','name','trade','changepercent']].sort_values('changepercent', ascending=False)
   # 遍历每支股票,下买单
+  items = {}
+  length = len(selected_codes.index)
+  date = datetime.now().strftime('%Y-%m-%d')
   for item in selected_codes.iterrows():
-    print(item[1]['code'],item[1]['name'])
+    # print(item[1]['code'],item[1]['name'])
+    items.setdefault('code',[])
+    items.setdefault('name',[])
+    items.setdefault('trans_type',['buy' for i in range(length)])
+    items.setdefault('price',[])
+    items.setdefault('count',[100 for i in range(length)])
+    items.setdefault('trans_date',[date for i in range(length)])
+    
+    items['code'].append(item[1]['code'])
+    items['name'].append(item[1]['name'])
+    items['price'].append(item[1]['trade'])
+
+  return items
+    
   '''
   ts.set_broker('zxjt', user='', passwd='')
   ts.get_broker()
@@ -30,5 +51,11 @@ def select_share():
   api.cancel(orderno='23232,2323',orderdate='20180115,20180116')
   api.deal_list(begin=20180115,end=20180116)
   '''
+
+from sqlalchemy import create_engine
 if __name__ == '__main__':
-  select_share()
+  engine = create_engine('mysql+pymysql://root@127.0.0.1/auto_stock?charset=utf8')
+  items = select_share()
+  if items:
+    df = pd.DataFrame.from_dict(items)
+    do_trans(df,engine)
